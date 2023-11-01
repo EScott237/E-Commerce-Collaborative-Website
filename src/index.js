8b27ef5c22b71c8478985430154ef0ec08c6834d
Erika Scott <Reeek@Erikas-MacBook-Pro.local>
2023-11-01 18:22:27 -0400

    Erika's Update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App";
+import "./index.css";
+
+ReactDOM.render(<NavBar />, document.getElementById("root"));
+
