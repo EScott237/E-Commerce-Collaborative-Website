4af2abca4fe3df162a0b31767e1fe481c227e55b
Aleska134 <143849441+Aleska134@users.noreply.github.com>
2023-11-22 18:39:00 +0000

    aleska 1

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -8,11 +8,12 @@ export default class MainContent extends Component{
     render()
     {
         return <div>
-            <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}</h4>
+            <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}
 
             <span className="badge badge-secondary m-2">{this.state.customersCount}</span>
 
             <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
+            </h4>
         </div>;
     }
     // Executes when the user clicks on the Refresh Button
