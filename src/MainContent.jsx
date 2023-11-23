730c272b7c37b26f6b87c3f2dd14e8779618f27e
Aleska134 <143849441+Aleska134@users.noreply.github.com>
2023-11-23 05:40:44 +0000

    Render Methods

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,23 +1,68 @@
 import React, {Component} from "react";
 
 export default class MainContent extends Component{
-    state = {pageTitle: "Customers", customersCount:5};
+    state = {
+        pageTitle: "Customers", 
+        customersCount:5,
+        customers:[
+            {id:1, name: "Scott",phone:"111-111",address:{city:"Miami"}},
+            {id:2, name: "Jones",phone:"222-222",address:{city:"New York"}},
+            {id:3, name: "Allen",phone:"333-333",address:{city:"California"}},
+            {id:4, name: "James",phone:null,address:{city:"Washington"}},
+            {id:5, name: "John",phone:null,address:{city:"Texas"}}
+        ]
+    };
 
+    render(){
+        return (
+        <div>
+            <h4 className="m-1 p-1">{this.state.pageTitle}
 
-
-    render()
-    {
-        return <div>
-            <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}
-
-            <span className="badge badge-secondary m-2">{this.state.customersCount}</span>
+            <span className="badge bg-secondary m-2">{this.state.customersCount}</span>
 
             <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
             </h4>
-        </div>;
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Customer Name</th>
+                        <th>Phone</th>
+                        <th>City</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   {this.getCustomerRow()}
+                </tbody>
+            </table>
+        </div>
+        );
     }
     // Executes when the user clicks on the Refresh Button
     onRefreshClick = () => {
         this.setState({customersCount:7});
+    };
+
+    getPhoneToRender = (phone) => {
+        if (phone) return phone;
+        else {
+            return <div className="bg-warning p-2 text-center">No phone</div>
+        }
     }
-}
+
+    getCustomerRow = () => {
+        return( this.state.customers.map((cust)=>{
+            return(
+                <tr key={cust.id}>
+                    <td>{cust.id}</td>
+                    <td>{cust.name}</td>
+                    <td>{this.getPhoneToRender(cust.phone)}</td>
+                    <td>{cust.address.city}</td>
+                </tr>  
+                    );
+                })
+                
+        );
+    }
+
+};
--- src/index.js
+++ src/index.js
@@ -7,5 +7,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import "./index.css";
 
+
+// New React Documentation (We have some console errors, I think it's for this :( ) 
+// website -> https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('app');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
